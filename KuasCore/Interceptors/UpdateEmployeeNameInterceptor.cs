d3b1f7d600564d519dd91d7fd05350885e41dc6f
s1101137202 <dodokey@hotmail.com.tw>
2015-04-17 23:40:07 +0800

    okkkkkk?

---
+++ KuasCore/Interceptors/UpdateEmployeeNameInterceptor.cs
@@ -0,0 +1,30 @@
+﻿
+using AopAlliance.Intercept;
+using KuasCore.Models;
+using System;
+using System.Diagnostics;
+
+
+namespace KuasCore.Interceptors
+{
+    class UpdateEmployeeNameInterceptor : IMethodInterceptor
+    {
+
+        public object Invoke(IMethodInvocation invocation)
+        {
+            Console.WriteLine("Interceptor 攔截到一個方法呼叫 = [{0}]", invocation);
+            Debug.Print("Interceptor 攔截到一個方法呼叫 = [{0}]", invocation);
+
+            object result = invocation.Proceed();
+            if (result is Employee)
+            {
+                Employee employee = (Employee)result;
+                employee.Name = employee.Name + "++++++++++++++++++++++++++++++++++++++++++";
+                result = employee;
+            }
+
+
+            return result;
+        }
+    }
+}
